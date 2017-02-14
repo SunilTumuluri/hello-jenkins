6192abaec45d562aa8f07543c8ac71ca56767414
SunilTumuluri <sunil.tumuluri@accenture.com>
2017-02-14 06:42:09 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var express = require('express');
  var app = express();
   
   app.get('/', function (req, res) {
-    res.send('hello Bangalore');
+    res.send('God Tussi Great HO');
     });
      
      app.listen(process.env.PORT || 5000);
